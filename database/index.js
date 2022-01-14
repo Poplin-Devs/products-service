17cee89ec1b415c726b8072f0ebed80823721a58
Leo <deutschleonardo@gmail.com>
2022-01-13 19:36:00 -0600

    server working and returning one product

--- database/index.js
+++ database/index.js
@@ -1,3 +1,50 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/SDC');
+mongoose.connect('mongodb://localhost:27017/SDC', () => {
+  console.log('Mongoose connected to MongoDB on port 27017');
+});
 
+const productSchema = new mongoose.Schema({
+  id: Number,
+  name: String,
+  slogan: String,
+  description: String,
+  category: String,
+  styles: [
+    {
+      id: Number,
+      name: String,
+      sale_price: Number,
+      original_price: Number,
+      photos: [
+        {
+          url: String,
+          thumbnail_url: String
+        }
+      ],
+      skus: [
+        {
+          size: String,
+          quantity: Number
+        }
+      ]
+    }
+  ],
+  relatedProducts: [Number],
+  features: [
+    {
+      feature: String,
+      value: String
+    }
+  ]
+});
+
+let Product = mongoose.model('Product', productSchema);
+
+let get = () => {
+  return Product.find({}).limit(1)
+  .then(product => {
+    return product;
+  });
+}
+
+module.exports = { get };
--- server/app.js
+++ server/app.js
@@ -1,9 +1,21 @@
 const express = require('express')
+const { get } = require('../database');
+
+
 const app = express()
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
+app.get('/', (req, res) => {
+  res.send('This is the Products Service!')
 })
+
+app.get('/product', (req, res) => {
+  return get()
+  .then(product => {
+    res.send(product);
+  })
+})
+
+module.exports = app;
